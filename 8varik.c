d3e6aaa47052774d31c1b368f16b766137712915
kos1kov <dima.kosikov00@mail.ru>
2019-03-31 23:43:25 +0300

    add error output

--- 8varik.c
+++ 8varik.c
@@ -6,31 +6,48 @@
 #include <libgen.h>
 #include <unistd.h>
 #define BUF_SIZE 1024
-typedef int           
-(*PFUNC)(const char*,         
-     const struct stat*);
+// typedef int           
+// (*PFUNC)(const char*,         
+//      const struct stat*);
 int             
 walk(const char*,       /* начальная директория */
-     PFUNC,char*, const char*);
+char*, const char*);
 
 const char *get_filename_ext(const char *filename) {
     const char *dot = strrchr(filename, '.');
     if(!dot || dot == filename) return "";
     return dot + 1;
 }
-int print_entry(const char* name, const struct stat* st)
-{
-    printf("%s\n", name);
-    return 0;
-}
+// int print_entry(const char* name, const struct stat* st)
+// {
+//     printf("%s\n", name);
+//     return 0;
+// }
+static char* programName;
 const char* pathnamefull;
 int main(int agrc, char *agrv[]){
     char *dirc, *basec, *bname, *dname;
-    dirc = strdup(agrv[0]);
-    basec = strdup(agrv[0]);
+    int nameLength = strlen(agrv[0]);
+
+    int slashPos = 0;
+
+    for(int i = nameLength - 1; i > 0; --i){
+        if(agrv[0][i] == '/'){
+            slashPos = i;
+            break;
+        }
+    }
+    if(slashPos != 0){
+        memcpy(agrv[0], &agrv[0][slashPos + 1], nameLength - slashPos - 1);
+        agrv[0][nameLength - slashPos - 1] = '\0';
+        programName = agrv[0];
+    }
+    else{
+        programName = agrv[0];
+    }
     pathnamefull = agrv[3];
-if (walk(agrv[1], print_entry,agrv[2],agrv[3]) ==1){
-    perror(basec);
+if (walk(agrv[1],agrv[2],agrv[3]) ==1){
+    perror(programName);
     return 0;
 }
 
@@ -38,7 +55,8 @@ return 1;
 }
 char buf[BUF_SIZE];
 char buf1[BUF_SIZE];
-int walk(const char* dirname, PFUNC pfunc,char* extens,const char* pathname)
+char *error;
+int walk(const char* dirname,char* extens,const char* pathname)
 {
     DIR* dp;
     struct stat st;
@@ -73,15 +91,19 @@ int walk(const char* dirname, PFUNC pfunc,char* extens,const char* pathname)
     
     if(stat(buf, &st) == -1)
     {
-        perror(dirp->d_name);
+        error = calloc(sizeof(programName)+len + +1,sizeof(char));
+        strcat(error, programName);
+        strcat(error, " ");
+        strcat(error, buf);
+        perror(error);
         retval = 1;
+        free(error);
         break;
     }
   
     if(S_ISDIR(st.st_mode))//1 if catalog exist
-    {
-        
-        if(walk(buf, pfunc,extens,buf1) == 1)
+    { 
+        if(walk(buf,extens,buf1) == 1)
         {
         retval = 1;
         break;
@@ -93,12 +115,18 @@ int walk(const char* dirname, PFUNC pfunc,char* extens,const char* pathname)
     const  char *name = dirp->d_name;
     strcat(buf1, name);
      const  char *ext = get_filename_ext(buf);
-     if (strcmp(extens,ext) == 0){
-         pfunc(dirp->d_name, &st); //print file
-         if(link(buf,buf1) == -1){
-           perror("gg");  
-         }
-     }
+      if (strcmp(extens,ext) == 0){
+    //      pfunc(dirp->d_name, &st); //print file
+          if(link(buf,buf1) == -1
+          ){
+              error = calloc(sizeof(programName)+len + sizeof(name)+1,sizeof(char));
+            strcat(error, programName);
+            strcat(error, " ");
+            strcat(error, buf);
+            perror(error);
+             free(error);
+          }
+      }
         
         
     }
